4a810452e85a78142fa875d17ff0a046569b4b58
Guy Klages <guy.klages@gmail.com>
2026-06-19 15:53:03 -0700

    backup and remove tutorial button

--- src/pages/index_original.js
+++ src/pages/index_original.js
@@ -7,7 +7,26 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
-
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();

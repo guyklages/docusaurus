f167433af0acb5f46fe2a6e2425d8361b182ac3b
Guy Klages <guy.klages@gmail.com>
2026-06-19 15:05:38 -0700

    added react.png

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -16,7 +16,7 @@ const FeatureList = [
   },
   {
     title: 'Technical Writer',
-    img: require('@site/static/img/undraw_docusaurus_react.svg'),
+    img: require('@site/static/img/undraw_docusaurus_react.png').default,
     description: (
       <>
         Developer guides <br/> <br/>

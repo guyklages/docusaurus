43de4f7833ca22078f3a70b507ec4c89a5838b85
Guy Klages <guy.klages@gmail.com>
2026-06-23 14:35:46 -0700

    Nallana

--- src/components/ScrollingBanner/index.js
+++ src/components/ScrollingBanner/index.js
@@ -16,6 +16,9 @@ export default function ScrollingBanner() {
         <div className={styles.bannerText}>
           "Guy consistently demonstrates a high level of professionalism, and dedication. He has a collaborative spirit, always willing to go the extra mile to ensure the success of our projects. Guy has a positive attitude and strong work ethic. It's been a pleasure working alongside Guy, and I am confident that he will continue to excel in his future endeavors." <br/><br/> <a href="https://www.linkedin.com/in/krantitalluri/" target="_blank" rel="noopener noreferrer">Kranti Talluri <br/> Manager at Nium</a>
         </div>
+        <div className={styles.bannerText}>
+          "I enjoyed working with Guy very much. His sense of humor and flexibility to join late-night meetings helped us finish before deadlines. His ability to turn vague product documents to developer friendly technical content is miraculously. I hope to work with him again in the future." <br/><br/> <a href="https://www.linkedin.com/in/abhishek-nallana/" target="_blank" rel="noopener noreferrer">Nallana Abhishek <br/> Coworker at Nium</a>
+        </div>
 
         {/* --- DUPLICATE SET FOR INFINITE LOOP --- */}
         <div className={styles.bannerText}>
@@ -26,6 +29,9 @@ export default function ScrollingBanner() {
         </div>
         <div className={styles.bannerText}>
           "Guy consistently demonstrates a high level of professionalism, and dedication. He has a collaborative spirit, always willing to go the extra mile to ensure the success of our projects. Guy has a positive attitude and strong work ethic. It's been a pleasure working alongside Guy, and I am confident that he will continue to excel in his future endeavors." <br/><br/> <a href="https://www.linkedin.com/in/krantitalluri/" target="_blank" rel="noopener noreferrer">Kranti Talluri <br/> Manager at Nium</a>
+        </div>
+        <div className={styles.bannerText}>
+          "I enjoyed working with Guy very much. His sense of humor and flexibility to join late-night meetings helped us finish before deadlines. His ability to turn vague product documents to developer friendly technical content is miraculously. I hope to work with him again in the future." <br/><br/> <a href="https://www.linkedin.com/in/abhishek-nallana/" target="_blank" rel="noopener noreferrer">Nallana Abhishek <br/> Coworker at Nium</a>
         </div>
 
       </div>

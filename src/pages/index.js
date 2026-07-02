63f7792d46e5deacddea940eb249009794267caf
Guy Klages <guy.klages@gmail.com>
2026-07-02 10:12:31 -0700

    emdash

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
         </Heading>
         <Heading as="h2" className="hero__subtitle">
           We're passionate about creating documentation and resources that help developers and end users fully unlock a product's
-          potential. We're also excited to support fellow technical writers--sharing interview questions, writing tips, and insights
+          potential. We're also excited to support fellow technical writers&emdash;sharing interview questions, writing tips, and insights
           to help you showcase your skills and land your next role with confidence.
         </Heading>
       </div>

c546651b3f8f60d8f586d60391c221639665df5b
Guy Klages <guy.klages@gmail.com>
2026-07-02 13:30:53 -0700

    Testimonial order and logo renaming

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -5,6 +5,14 @@
  * adding a matching entry to make it appear.
  */
 export default {
+  Microsoft: {
+    company: 'Microsoft',
+    quote:
+      'Guy joined our team at Microsoft for a short engagement to help build out our content system. He brought deep technical writing expertise and a strong command of source control practices. Throughout the project, Guy showed remarkable patience and flexibility while navigating complex requirements, delivering results despite a tight timeline.',
+    person: 'Louie Mayor',
+    role: 'Manager',
+    link: 'https://www.linkedin.com/in/louie-mayor/',
+  },
   FIS: {
     company: 'FIS Global',
     quote:
@@ -21,7 +29,7 @@ export default {
     role: 'Manager',
     link: 'https://www.linkedin.com/in/krantitalluri/',
   },
-  'Northeastern-seal': {
+  Northeastern: {
     company: 'Northeastern University',
     quote:
       'Based on my work with Guy for the Pronunciation Workshops at Northeastern University, I highly recommend him as a language teacher. He joined the workshops after the start of the semester but he jumped right in with confidence, built a good rapport with the students, and got them speaking right away. His knowledge of both technology and language instruction results in some very creative and interactive lessons that integrate audio and visual components.',
@@ -37,7 +45,7 @@ export default {
     role: 'Manager',
     link: 'https://www.linkedin.com/in/pimvandevis/',
   },
-  chargepoint: {
+  ChargePoint: {
     company: 'ChargePoint',
     quote:
       'Guy is very passionate about the EV industry and helping people through training, documentation and writing. He looks for ways to improve content delivery to a variety of audiences, always exploring, evaluating and evolving. He was a tireless advocate for end users of our software and services. It was a privilege to have Guy on my team, and I recommend him highly as a technical communicator and writer.',
@@ -45,15 +53,7 @@ export default {
     role: 'Manager',
     link: 'https://www.linkedin.com/in/laynenorthsea/',
   },
-  'microsoft-window': {
-    company: 'Microsoft',
-    quote:
-      'Guy joined our team at Microsoft for a short engagement to help build out our content system. He brought deep technical writing expertise and a strong command of source control practices. Throughout the project, Guy showed remarkable patience and flexibility while navigating complex requirements, delivering results despite a tight timeline.',
-    person: 'Louie Mayor',
-    role: 'Manager',
-    link: 'https://www.linkedin.com/in/louie-mayor/',
-  },
-  va: {
+  VA: {
     company: 'VA Medical Center',
     quote:
       'Guy worked to improve our existing user guides, update our training materials and deliver in-person training which he recorded and edited for use by those that could not attend the scheduled training sessions. He is congenial, detail-oriented and could always be counted on to provide a unique perspective on ways to improve the eDC product set. I enjoyed working with him and hope to work with him again in the future.',
@@ -66,7 +66,7 @@ export default {
     quote:
       "It was so great working with Guy. Not only was he very detailed and always improving the database he was creating to fit our ever-changing needs, he was also always available and very quick to respond to any issue we were experiencing. Guy was always patient with those of us with less computer savy in explaining the new features he was constantly adding, and we wouldn't have gotten the job done without his help.",
     person: 'Allyn Anderson',
-    role: 'Coworker',
+    role: 'Team lead',
     link: 'https://www.linkedin.com/in/allyn-anderson-adp',
   },
 };
